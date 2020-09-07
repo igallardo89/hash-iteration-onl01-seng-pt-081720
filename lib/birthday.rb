0697369d2ee092e641e73b0cde4fc1c0eea6e0b4
Illiana Gallardo <igallardo0716@gmail.com>
2020-09-07 17:49:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,5 +10,16 @@ birthday_kids.each do |kids_name, age|
   end
 end
 
-
-
+def age_appropriate_birthday(birthday_kids)
+           birthday_kids.each do |kid, age|
+             if age <= 12
+             puts "Happy Birthday #{kid}! You are now #{age} years old!"
+           birthday_kids
+           else
+           puts "You are too old for this."
+         end 
+          birthday_kids
+       end
+     end
+             
+            
